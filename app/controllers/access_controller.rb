a22cefbd7376fafee172eb8d9ce011e8270e1cc3
Zsolt Ban <banzsolt94@gmail.com>
2016-12-12 20:25:10 +0000

    Reworked the highscore main page

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -7,7 +7,14 @@ class AccessController < ApplicationController
       game_id = params[:id]
     end
 
+    @game = Game.find(game_id)
+    @players = @game.players.where('public = ?', true)
 
+    @players.each do |player|
+      player.count_score
+    end
+
+    @players = @players.sort_by{|data| data.win_per_loss}
 
   end
 
--- app/models/player.rb
+++ app/models/player.rb
@@ -6,6 +6,33 @@ class Player < ApplicationRecord
   has_many :player_awards, :dependent => :delete_all
   has_many :game_session_players, :dependent => :delete_all
 
+  has_many :game_sessions, through: :game_session_players
+
   belongs_to :game
+
+  attr_accessor :win_count, :loss_count, :win_per_loss
+
+  def count_score
+
+    self.win_count = 0
+    self.loss_count = 0
+    self.win_per_loss = 0
+
+    self.game_sessions.where('finished = ?', true).each do |game_session|
+      player_data = game_session.game_session_players.where('player_id = ?', self.id).first
+
+      if player_data.won
+        self.win_count += 1
+      else
+        self.loss_count += 1
+      end
+    end
+
+    if self.win_count != 0 && self.loss_count != 0
+      self.win_per_loss = self.win_count / self.loss_count
+    end
+
+
+  end
 
 end
