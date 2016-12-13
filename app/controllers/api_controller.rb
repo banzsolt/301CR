acbb65c66f508ac5948f4065f3a03fab4fe7c1a4
Zsolt Ban <banzsolt94@gmail.com>
2016-12-13 19:59:39 +0000

    Player win/loss added safety check on the save function. Added possibility to create new game sessions which are required params for the win/loss function

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,13 @@
 class ApiController < ApplicationController
 
-  before_action :authorised, :except => [:index]
+  before_action :authorised, :except => [:index, :register, :player_info]
 
   def index
   end
 
   def playerWon
+
+    check_game_session
 
     @player.win += 1;
 
@@ -15,13 +17,19 @@ class ApiController < ApplicationController
       @player.win_loss = @player.win / @player.loss
     end
 
-    @player.save
-
-    render :json => @player, :except => :password_digest
+    if @player.save
+      @game_session.finished = true
+      @game_session.save
+      render :json => @player, :except => :password_digest
+    else
+      render :json => {'error':'there was an error while trying to save the player'}
+    end
 
   end
 
   def playerLost
+
+    check_game_session
 
     @player.loss += 1;
 
@@ -31,9 +39,91 @@ class ApiController < ApplicationController
       @player.win_loss = @player.win / @player.loss
     end
 
-    @player.save
+    if @player.save
+      @game_session.finished = true
+      @game_session.save
+      render :json => @player, :except => :password_digest
+    else
+      render :json => {'error':'there was an error while trying to save the player'}
+    end
+
+  end
+
+  def create_game_session
+
+    game_session = GameSession.new
+
+    if params[:game_id].nil?
+      render :json => {'error': 'game_id missing from parameters'}
+    end
+
+    render :json => game_session
+
+  end
+
+  def register
+
+    if request.headers['email'].nil? || request.headers['password'].nil?
+      render :json => {'error': 'The header is missing either email or password'}
+      return false
+    end
+
+    player = Player.where('email = ?', request.headers['email']).first
+    if !player.nil?
+      render :json => {'error': 'User with this email already registered'}
+      return false
+    end
+
+    player = Player.new
+    player.email = request.headers['email']
+    player.password = request.headers['password']
+    player.demographic = 'UK'
+    if !params[:demographic].nil?
+      player.demographic = params[:demographic]
+    end
+
+  end
+
+  def player_info
+
+    player_id = 1
+    if !params[:player_id].nil?
+      player_id = params[:player_id]
+    end
+
+    @player = Player.find(player_id)
 
     render :json => @player, :except => :password_digest
 
   end
+
+  def high_score
+
+
+
+  end
+
+  private
+
+    def check_game_session
+
+      if params[:game_session_id].nil?
+        render :json => {'error':'game_sessio_id missing from the parameters'}
+        return false
+      end
+
+      @game_session = GameSession.find(params[:game_session_id])
+
+      if @game_session.nil?
+        render :json => {'error':'no game sessions with this ID'}
+        return false
+      end
+
+      if @game_session.finished
+        render :json => {'error': 'game session already finished'}
+        return false
+      end
+
+    end
+
 end
